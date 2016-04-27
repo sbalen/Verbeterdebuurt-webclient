d7a8b2e73894226f040c0f865483a3cb591ddb5c
hendrikus andrianto <hendrikus andrianto>
2016-04-27 14:35:06 +0700

    give spacing for footer, change report to dutch, add detail at issue detail

--- js/vdb-start.js
+++ js/vdb-start.js
@@ -8,7 +8,9 @@ function start(){this._map_center = {lat: 52.158367, lng: 4.492999};
         streetViewControl: false, // hide the yellow Street View pegman
         /*scaleControl: false, // allow users to zoom the Google Map*/
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center:  this._map_center
+        center:  this._map_center,
+        zoomControlOptions : { 
+          position :google.maps.ControlPosition.RIGHT_CENTER }
       };
      map = new google.maps.Map(document.getElementById('googlemaps'), mapOptions);
      getLocation(map);
@@ -18,6 +20,7 @@ function start(){this._map_center = {lat: 52.158367, lng: 4.492999};
         geocodeAddress(geocoder, map);
         cityName=null;
      }
+
       //marker google map
        // for(var i= 0 ; i < issuesData.count ; i++){
        //               var latLng = {lat:issues.issues[i].location.latitude , lng : issues.location.longitude}
